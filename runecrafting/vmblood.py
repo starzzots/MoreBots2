b826e6bfe5d582b5f7a99922d386389a8ba89ec2
Kyle <vboxuser@botting1.(none)>
2025-06-29 15:47:44 -0500

    I think its finished

--- runecrafting/vmblood.py
+++ runecrafting/vmblood.py
@@ -18,7 +18,7 @@ from tools.tools import *
 from time import time, sleep
 import keyboard
 SCREEN_SIZE = (0,0,1200,900)
-stuck_state = (0,2,0)
+STUCK_STATE = (0,2,0)
 pos_1 = (255,250,0)
 pos_2=(73,64,52)
 pos_3=(0,1,0)
@@ -42,16 +42,17 @@ def world_hopper():
     sleep(2.5)
 
 def main():
-    stuck_state = pos_4
+    STUCK_STATE = (0,2,0)
     center = pg.pixel(502,468)
     #pg.displayMousePosition()
     #print(center)
     if pg.pixel(944,640) != (53,53,255):
         Randomize((904,908,630,632)).randleft()
         sleep(.2)
-    if stuck_state == pos_4:
+        return 0
+    if STUCK_STATE == pos_4:
         if center == pos_1:
-            stuck_state = pos_4
+            STUCK_STATE = pos_4
             try:
                 bank_click = findobjat((0,0,171), SCREEN_SIZE, delta1x=-5,delta2x=-5,delta1y=5,delta2y=5)
                 Randomize(bank_click).randleft()
@@ -60,25 +61,25 @@ def main():
                 print("where is the bank?")
             return 1
         elif center == pos_2:
-            stuck_state = pos_4
+            STUCK_STATE = pos_4
             #pg.displayMousePosition()
             Randomize((271,281,74,84)).randleft() #tab2 in bank
-            sleep(.8)
+            sleep(.3)
             Randomize((202,210,112,120)).randleft() # click essense in bank
-            sleep(.7)
+            sleep(.3)
             Randomize((861,869,634,641)).randleft() #slot 2 in invo
-            sleep(.8)
+            sleep(.3)
             Randomize((202,210,112,120)).randleft() # click essense in bank
-            sleep(.7)
+            sleep(.3)
             Randomize((861,869,634,641)).randleft() #slot 2 in invo
-            sleep(.8)
+            sleep(.3)
             Randomize((202,210,112,120)).randleft() # click essense in bank
-            sleep(.7)
+            sleep(.3)
             Randomize((597,607,34,44)).randleft() # exit bank button
-            sleep(.7)
+            sleep(.3)
             return 0
         elif center == pos_3:
-            stuck_state = pos_4
+            STUCK_STATE = pos_4
             try:
                 trapdoor_click = findobjat((125,0,207), delta1x=0,delta2x=0,delta1y=5,delta2y=6)
                 Randomize(trapdoor_click).randleft()
@@ -87,32 +88,38 @@ def main():
                 print("Where is the trapdoor...")
             return 0
         elif center == pos_4:
-            stuck_state = pos_4
+            STUCK_STATE = pos_4
             #pg.displayMousePosition()
             try:
-                secret_door_click = findobjat((13,13,255), SCREEN_SIZE, delta1x=0,delta2x=0,delta1y=5,delta2y=5)
+                secret_door_click = findobjat((13,13,255), SCREEN_SIZE, delta1x=2,delta2x=2,delta1y=3,delta2y=3)
                 Randomize(secret_door_click).randleft()
                 sleep(5.8)
             except:
                 print("IM STUCK AHHHHHH!")
             return 0
         elif center == pos_5:
-            stuck_state == pos_5
+            STUCK_STATE == pos_5
             Randomize((594,612,640,653)).randleft()
-            sleep(8)
+            sleep(8.5)
+            try:
+                cave_click = findobjat((0,107,44), SCREEN_SIZE, delta1x=0,delta2x=0,delta1y=0,delta2y=0)
+                Randomize(cave_click).randleft()
+                sleep(5)
+            except:
+                print("whyyyyyyyyyyyyyyyyyyyyyy")
             return 0
         elif center == pos_6:
-            stuck_state=pos_4
+            STUCK_STATE=pos_4
             Randomize((532,534,433,441)).randleft()
             sleep(3.6)
             return 0
         elif center == pos_7:
-            stuck_state=pos_4
+            STUCK_STATE=pos_4
             Randomize((640,644,391,397)).randleft()
             sleep(6.3)
             return 0
         elif center == pos_8:
-            stuck_state = pos_4
+            STUCK_STATE = pos_4
             Randomize((440,446,463,468)).randleft()
             sleep(3.2)
             Randomize((858,868,635,640)).randleft()#second bag slot
@@ -125,8 +132,9 @@ def main():
             sleep(.2)
             Randomize((818,826,633,639)).randleft()#first bag slot
             sleep(5.2)
+            return 0
         elif center == pos_9:
-            stuck_state = pos_4
+            STUCK_STATE = pos_4
             Randomize((480,486,437,440)).randleft()
             sleep(2.6)
             Randomize((428,430,428,434)).randleft()
@@ -134,26 +142,29 @@ def main():
             return 0
         else:
             try:
-                secret_door_click = findobjat((13,13,255), SCREEN_SIZE, delta1x=0,delta2x=0,delta1y=5,delta2y=5)
+                secret_door_click = findobjat((13,13,255), SCREEN_SIZE, delta1x=2,delta2x=2,delta1y=3,delta2y=3)
                 Randomize(secret_door_click).randleft()
                 sleep(5.8)
             except:
                 print("IM STUCK AHHHHHH!")
+            return 0
 
-    elif stuck_state == pos_5:
+    if STUCK_STATE == pos_5:
         if center == pos_6:
-            stuck_state=pos_4
+            STUCK_STATE=pos_4
             Randomize((532,534,433,441)).randleft()
             sleep(3.6)
             return 0
-        try:
-            cave_click = findobjat((0,107,44), SCREEN_SIZE, delta1x=0,delta2x=0,delta1y=5,delta2y=5)
-            Randomize(cave_click).randleft()
-            sleep(5)
-        except:
-            print("whyyyyyyyyyyyyyyyyyyyyyy")
-        return 0
-count = 0
+        else:
+            STUCK_STATE = pos_5
+            try:
+                cave_click = findobjat((0,107,44), SCREEN_SIZE, delta1x=0,delta2x=0,delta1y=5,delta2y=5)
+                Randomize(cave_click).randleft()
+                sleep(5)
+            except:
+                print("whyyyyyyyyyyyyyyyyyyyyyy")
+            return 0
+count = 79
  
 while True:
     count = count + main()
@@ -162,7 +173,7 @@ while True:
         sys.exit(0)  # Exit the script when ESC is pressed
         break
     main()
-    if count == 80:
+    if count >= 80:
         world_hopper()
         count = 0
     
